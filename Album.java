b4d84ad489896a923871496f1a603709576fa0af
zolti1015 <zkotha@arizona.edu>
2025-02-26 14:59:01 -0700

    Update Album.java

--- Album.java
+++ Album.java
@@ -1,1 +1,17 @@
+import java.util.ArrayList;
+
+
+public class Album {
+
+	
+	private ArrayList<Song> songs;
+	private String title;
+	private String artist;
+	private String Genre;
+	private String year;
+	
+	public Album (String title, String artist, String genre, String year) {
+		this.title = title;
+	}
+}
 
