3de971b1bf95a3fa52a21d5ab871e39d69a7128c
zolti1015 <zkotha@arizona.edu>
2025-02-27 12:52:04 -0700

    Update LibraryModel.java

--- LibraryModel.java
+++ LibraryModel.java
@@ -4,11 +4,9 @@ public class LibraryModel {
 	
 	ArrayList<Album> albums = new ArrayList<>();
 	ArrayList<Song> songs = new ArrayList<>();
+	ArrayList<Playlist> playlists = new ArrayList<>();
 	
-	public LibraryModel () {
-		
-		
-	}
+	public LibraryModel () { }
 	
 	   // get song info by title or artist
 		public String getSongInfo(String titleOrArtist) {
@@ -19,25 +17,34 @@ public class LibraryModel {
 					if (song.getArtist().equals(titleOrArtist) ||
 						song.getTitle().equals(titleOrArtist) ) 
 					{
-							return song.toString() +
-									"\nAlbum: " + album.getTitle();
+							System.out.println(song.toString() +
+									"\nAlbum: " + album.getTitle());
 				    }
-			   }
-		   }
+			    }
+		    }
 		return "Searched for Data is not in the database."; // need thing not found message
 }
 		
-		// print album info and list of songs in order if found 
+		// get album info and list of songs by title or Artist
 		public String getAlbumInfo(String titleOrArtist) {
 			for (Album album : albums) {
 				if (album.getTitle().equals(titleOrArtist) ||
 					album.getArtist().equals(titleOrArtist)) {
-					return album.toString();
+					System.out.println(album.toString()); // printing here, so if multiple results loop will continue to search
 				}
-				}
+			}
 		return "Searched for Data is not in the database.";
 		}
 		
+		
+		// find playlist by provided name and print song details
+		public void searchForPlaylist(String playlistName) {
+			for (Playlist playlist : playlists) {
+				if(playlist.getName().equals(playlistName)) {
+					System.out.println(playlist.toString());
+				}
+			}
+		}
 		public ArrayList<Song> getSongs () {
 			return new ArrayList<Song>(songs); // copy list to avoid reference
 		}
@@ -46,9 +53,6 @@ public class LibraryModel {
 			return new ArrayList<Album>(albums); // copy list to avoid reference
 		}
 		
-	
-	
-	
 	//  keeps track of the user’s library 
 	// and interacts with other classes including the View and the MusicStore. 
 	
