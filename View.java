5be195fe05579265df080e46f0782436bc2bfee5
zolti1015 <zkotha@arizona.edu>
2025-02-27 12:54:14 -0700

    Update View.java

--- View.java
+++ View.java
@@ -1,4 +1,8 @@
 /*
+
+**** NOTE THE HORIZONTAL LINES MEAN HAS BEEN IMPLEMENTED BY SOME METHOD AT LEAST IN THEORY BUT MIGHT NEED TO BE CHANGED*****
+
+
 search for information from the music store 
 ● for a song by title ------------------------------------------
 ● for a song by artist -------------------------------------------
@@ -10,15 +14,18 @@ the expected results of searching
 ● for a song that is in the database: print the song title, the artist, and the album it’s on  -----------------------
 ● for an album: print the album information and a list of the songs in the appropriate order --------------------------
 ● for anything that is not in the database: a message indicating that the item is not there  ----------------------------
-● for anything that has multiple results: print all the results 
+● for anything that has multiple results: print all the results  -----------------------------------------------------------
 
 
 search for information from the user library 
-● should cover all the search cases listed for the music store 
+● should cover all the search cases listed for the music store  -------------------------------------
 ● should also be able to search for a playlist by name – the result should print the songs 
-(title and artist) 
+(title and artist)  -------------------------------------------------------------------------------------
 
 
+
+// TO IMPLEMENT:
+ 
 add something to the library 
 ● add a song to the library (as long as it is in the store) 
 ● add a whole album to the library (as long as it is in the store) 
@@ -37,14 +44,26 @@ create a playlist and add/remove songs
 ● songs should be maintained in the order they are added 
 
 
-mark a song as “favorite” 
+mark a song as “favorite” -------------------------------------------
 
 rate a song  
-● the ratings are 1 to 5 
-● songs do not have to be rated so there is no default rating 
-● songs that are rated as 5 should automatically be set to “favorite”
+● the ratings are 1 to 5 ------------------------------------------------
+● songs do not have to be rated so there is no default rating ---------------------------------
+● songs that are rated as 5 should automatically be set to “favorite” ---------------------------------
  * 
  */
 public class View {
-
+	// As indicated in Part 2, the view is simply the user interface, and its only purpose is to interact 
+	// with the user and communicate with the model. It should not be storing or manipulating any of 
+	// the data. It simply gets user requests and gets information from the model based on those 
+	// requests. 
+	
+	private final MusicStore musicstore = new MusicStore();
+	private final LibraryModel library = new LibraryModel();
+	
+	public static void main(String[] args) {
+			
+	}
+	
 }
+
