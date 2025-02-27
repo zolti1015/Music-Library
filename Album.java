23b50070814c45ac0ddffe0fec491e0a8eee4e4c
zolti1015 <zkotha@arizona.edu>
2025-02-27 10:28:02 -0700

    Update Album.java

--- Album.java
+++ Album.java
@@ -1,17 +1,18 @@
 import java.util.ArrayList;
 
+
 public class Album {
 
 	private ArrayList<Song> songs;
 	private String title;
 	private String artist;
-	private String Genre;
+	private String genre;
 	private String year;
 	
 	public Album (String title, String artist, String genre, String year) {
 		this.title = title;
 		this.artist = artist;
-		this.Genre = genre;
+		this.genre = genre;
 		this.year = year;
 	}
 	
@@ -19,4 +20,37 @@ public class Album {
 	public void addSong (String title) {
 		this.songs.add(new Song(title));
 	}
+	
+	public ArrayList<Song> getSongs() {
+		return new ArrayList<Song>(songs);
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getArtist() {
+		return artist;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+	
+	public String getYear() {
+		return year;
+	}
+	
+	public String toString() {
+		String listOfSongs = "\nSongs on album: \n";
+		for (Song song : songs) {
+			listOfSongs += song.getTitle() + "\n";
+		}
+		return "Title: " + title +
+				"\nArtist: " + artist + 
+				"\nGenre: " + genre + 
+				"\nYear: " + year + 
+				listOfSongs;
+	}
+	
 }
