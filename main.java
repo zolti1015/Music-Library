307303ea8d9e4f471fa24e871d7bdb99cb858079
Always-Done <emartinez37@arizona.edu>
2025-02-28 16:53:35 -0700

    Update main.java

--- main.java
+++ main.java
@@ -1,11 +1,1 @@
-import java.io.IOException;
 
-
-public class Main {
-	
-  public static void main(String[] args) throws IOException {
-    LibraryModel library = new LibraryModel();
-    View view = new View(library);
-    view.run();
-  }
-}
