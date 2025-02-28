af7024c328fd114c22a3fb248be0af271e6be7eb
Always-Done <emartinez37@arizona.edu>
2025-02-28 16:16:51 -0700

    Update main.java

--- main.java
+++ main.java
@@ -2,6 +2,6 @@ public class Main {
   public static void main(String[] args) {
     LibraryModel library = new LibraryModel();
     View view = new View(library);
-    view.start();
+    view.run();
   }
 }
