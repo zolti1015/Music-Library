f03970bb36b36e099dc91d1a2bba4796166187b2
Always-Done <emartinez37@arizona.edu>
2025-02-28 14:46:22 -0700

    Update View.java
    menus

--- View.java
+++ View.java
@@ -1,27 +1,23 @@
-
 import java.util.List;
 import java.util.Scanner;
-
 
 public class View {
     private LibraryModel model;
     private Scanner scanner;
-
 
     public View(LibraryModel model) {
         this.model = model;
         this.scanner = new Scanner(System.in);
     }
 
-
     public void run() {
         displayWelcomeMessage();
-        
+
         while (true) {
             displayMainMenu();
             System.out.println("Enter your choice: ");
             int choice = scanner.nextInt();
-            
+
             switch (choice) {
                 case 0:
                     System.out.println("Exiting... Goodbye.");
@@ -43,7 +39,7 @@ public class View {
                     break;
             }
         }
-        
+
         displayGoodbyeMessage();
     }
 
@@ -64,42 +60,75 @@ public class View {
         System.out.println("2. Search for song by artist in music store");
         System.out.println("3. Search for album by title in music store");
         System.out.println("4. Search for album by artist in music store");
-        
+
         System.out.println("5. Search for song by title in library");
         System.out.println("6. Search for song by artist in library");
         System.out.println("7. Search for album by title in library");
         System.out.println("8. Search for album by artist in library");
-        
+
         System.out.println("9. Search for playlist by name");
     }
 
+    private void displayAddMenu() {
+        System.out.println("\nADD MENU");
+        System.out.println("1. Add song to library");
+        System.out.println("2. Add album to library");
+        System.out.println("0. Back to main menu");
+    }
+
+    private void displayListMenu() {
+        System.out.println("\nLIST MENU");
+        System.out.println("1. List all songs");
+        System.out.println("2. List all artists");
+        System.out.println("3. List all albums");
+        System.out.println("4. List all playlists");
+        System.out.println("5. List favorite songs");
+        System.out.println("0. Back to main menu");
+    }
+
+    private void displayPlaylistMenu() {
+        System.out.println("\nPLAYLIST MENU");
+        System.out.println("1. Create new playlist");
+        System.out.println("2. Add song to playlist");
+        System.out.println("3. Remove song from playlist");
+        System.out.println("4. View playlist");
+        System.out.println("0. Back to main menu");
+    }
+
+    private void displaySongActionsMenu() {
+        System.out.println("\nSONG ACTIONS MENU");
+        System.out.println("1. Mark song as favorite");
+        System.out.println("2. Rate song");
+        System.out.println("0. Back to main menu");
+    }
+
     private void handleSearchMenu() {
-    
+
         while (true) {
             displaySearchMenu();
             System.out.println("Enter your choice: ");
             int choice = scanner.nextInt();
-            
+
             switch (choice) {
                 case 0:
-                	return;
+                    return;
                 case 1:
                     System.out.println("Enter title: ");
                     String title = scanner.nextLine();
                     System.out.println(model.getStore().getSongInfo(title));
                     break;
-                case 2: 
-                	 System.out.println("Enter artist: ");
-                     String artist = scanner.nextLine();
-                     System.out.println(model.getStore().getSongInfo(artist));
-                     break;
+                case 2:
+                    System.out.println("Enter artist: ");
+                    String artist = scanner.nextLine();
+                    System.out.println(model.getStore().getSongInfo(artist));
+                    break;
                 case 3:
-                	 System.out.println("Enter title: ");
-                     String title2 = scanner.nextLine();
-                     System.out.println(model.getStore().getSongInfo(title2));
-                     break;
+                    System.out.println("Enter title: ");
+                    String title2 = scanner.nextLine();
+                    System.out.println(model.getStore().getSongInfo(title2));
+                    break;
                 case 4:
-               	 	System.out.println("Enter artist: ");
+                    System.out.println("Enter artist: ");
                     String artist2 = scanner.nextLine();
                     System.out.println(model.getStore().getSongInfo(artist2));
                     break;
@@ -108,28 +137,27 @@ public class View {
                     String title3 = scanner.nextLine();
                     System.out.println(model.getStore().getSongInfo(title3));
                     break;
-                case 6: 
-                	 System.out.println("Enter artist: ");
-                     String artist3 = scanner.nextLine();
-                     System.out.println(model.getSongInfo(artist3));
-                     break;
+                case 6:
+                    System.out.println("Enter artist: ");
+                    String artist3 = scanner.nextLine();
+                    System.out.println(model.getSongInfo(artist3));
+                    break;
                 case 7:
-                	 System.out.println("Enter title: ");
-                     String title4 = scanner.nextLine();
-                     System.out.println(model.getSongInfo(title4));
-                     break;
+                    System.out.println("Enter title: ");
+                    String title4 = scanner.nextLine();
+                    System.out.println(model.getSongInfo(title4));
+                    break;
                 case 8:
-               	 	System.out.println("Enter artist: ");
+                    System.out.println("Enter artist: ");
                     String artist4 = scanner.nextLine();
                     System.out.println(model.getSongInfo(artist4));
                     break;
             }
         }
     }
-    
+
     public void displayWelcomeMessage() {
-    	System.out.println("Welcome to your library.");
+        System.out.println("Welcome to your library.");
     }
-    
-    
+
 }
