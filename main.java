6ae4359b5642441bf8a59d4f516d2e0394dcff42
zolti1015 <zkotha@arizona.edu>
2025-02-28 16:39:35 -0700

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
+import java.io.IOException;
+
+
 public class Main {
-  public static void main(String[] args) {
+	
+  public static void main(String[] args) throws IOException {
     LibraryModel library = new LibraryModel();
     View view = new View(library);
     view.run();
