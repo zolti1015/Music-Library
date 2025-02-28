41f5ad83955488d594a08f9b7281789f62598024
Always-Done <emartinez37@arizona.edu>
2025-02-28 16:55:45 -0700

    Update Song.java
    Song class

--- Song.java
+++ Song.java
@@ -1,1 +1,20 @@
+public class Song {
+    private String title;
+    private String artist;
+    private String albumTitle;
 
+    public Song(String title, String artist, String albumTitle) {
+        this.title = title;
+        this.artist = artist;
+        this.albumTitle = albumTitle;
+    }
+
+    public String getTitle() { return title; }
+    public String getArtist() { return artist; }
+    public String getAlbumTitle() { return albumTitle; }
+
+    @Override
+    public String toString() {
+        return title + " - " + artist + " (Album: " + albumTitle + ")";
+    }
+}
