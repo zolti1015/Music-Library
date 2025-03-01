c6e499010fe03f3303eb5c2f50ebd744899d13a5
zolti1015 <zkotha@arizona.edu>
2025-02-28 19:25:34 -0700

    Update Song.java

--- Song.java
+++ Song.java
@@ -1,61 +1,297 @@
+import java.util.Scanner;
 
-public class Song {
-	
-	private final String title;
-	private String artist;
-	public enum Rating {ONE, TWO, THREE, FOUR, FIVE}
-	private Rating rating;
-	private boolean favorite;
-	
-	public Song(String title, String artist) {
-		this.title = title;
-		this.artist = artist;
-	}
-	
-	public void rateSong(int ratingOneToFive) {
-		Rating rating;
-		switch (ratingOneToFive) {
-			case 1: 
-				rating = Rating.ONE;
-			case 2: 
-				rating = Rating.TWO;
-			case 3: 
-				rating = Rating.THREE;
-			case 4: 
-				rating = Rating.FOUR;
-			case 5: 
-				rating = Rating.FIVE;
-		// 5 star rating makes song automatic favorite
-		if (rating == Rating.FIVE) favorite = true;
-		this.rating = rating;
-		}
-	}
-	public String getTitle() {
-		return title;
-	}
-	
-	public String getArtist() {
-		return artist;
-	}
-	
-	public String toString() {
-		
-		return "Title: " + title + 
-				"\nArtist: " + artist;
-	}
-	
-	public void makeFavorite() {
-		this.favorite = true;
-	}
-	
-	public boolean getFavStatus() {
-		return this.favorite;
-	}
-	
-	public boolean equals(Song song) {
-		if (song.getTitle().equals(title) && song.getArtist().equals(artist)) {
-			return true;
-		}
-		return false;
-	}
+
+public class View {
+    private LibraryModel model;
+    private Scanner scanner;
+
+
+    public View(LibraryModel model) {
+        this.model = model;
+        this.scanner = new Scanner(System.in);
+    }
+
+
+    public void run() {
+        displayWelcomeMessage();
+        
+        while (true) {
+            displayMainMenu();
+            System.out.println("Enter your choice: ");
+            int choice = scanner.nextInt();
+            
+            switch (choice) {
+                case 0:
+                    System.out.println("Exiting... Goodbye.");
+                    return;
+                case 1:
+                    handleSearchMenu();
+                    break;
+                case 2:
+                    handleAddMenu();
+                    break;
+                case 3:
+                    handleListMenu();
+                    break;
+                case 4:
+                    handlePlaylistMenu();
+                    break;
+                case 5:
+                	// favorite and rate
+                    handleSongActionsMenu();
+                    break;
+            }
+        }
+    }
+
+    private void displayMainMenu() {
+        System.out.println("\nMAIN MENU");
+        System.out.println("1. Search for music");
+        System.out.println("2. Add to library");
+        System.out.println("3. List library items");
+        System.out.println("4. Manage playlists");
+        System.out.println("5. Song actions (favorite/rate)");
+        System.out.println("0. Exit");
+    }
+
+    private void displaySearchMenu() {
+        System.out.println("\nSEARCH MENU");
+        System.out.println("0. Go back to main menu");
+        System.out.println("1. Search for song by title in music store");
+        System.out.println("2. Search for song by artist in music store");
+        System.out.println("3. Search for album by title in music store");
+        System.out.println("4. Search for album by artist in music store");
+        
+        System.out.println("5. Search for song by title in library");
+        System.out.println("6. Search for song by artist in library");
+        System.out.println("7. Search for album by title in library");
+        System.out.println("8. Search for album by artist in library");
+    }
+    
+    
+    private void displayAddMenu() {
+        System.out.println("\nADD MENU");
+        System.out.println("1. Add song to library");
+        System.out.println("2. Add album to library");
+        System.out.println("0. Back to main menu");
+    }
+
+    private void displayListMenu() {
+        System.out.println("\nLIST MENU");
+        System.out.println("1. List all songs");
+        System.out.println("2. List all artists");
+        System.out.println("3. List all albums");
+        System.out.println("4. List all playlists");
+        System.out.println("5. List favorite songs");
+        System.out.println("0. Back to main menu");
+    }
+
+    private void displayPlaylistMenu() {
+        System.out.println("\nPLAYLIST MENU");
+        System.out.println("1. Create new playlist");
+        System.out.println("2. Add song to playlist");
+        System.out.println("3. Remove song from playlist");
+        System.out.println("4. View playlist");
+        System.out.println("0. Back to main menu");
+    }
+
+    private void displaySongActionsMenu() {
+        System.out.println("\nSONG ACTIONS MENU");
+        System.out.println("1. Mark song as favorite");
+        System.out.println("2. Rate song");
+        System.out.println("0. Back to main menu");
+    }
+
+    private void handleSearchMenu() {
+    
+        while (true) {
+            displaySearchMenu();
+            System.out.println("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // consume the leftover newline character
+            
+            switch (choice) {
+                case 0:
+                	return;
+                case 1:
+                     System.out.println("Enter title: ");
+                     String title = scanner.nextLine();
+                     System.out.println(model.getStore().getSongInfo(title));
+                     break;
+                case 2: 
+                	 System.out.println("Enter artist: ");
+                     String artist = scanner.nextLine();
+                     System.out.println(model.getStore().getSongInfo(artist));
+                     break;
+                case 3:
+                	 System.out.println("Enter title: ");
+                     String title2 = scanner.nextLine();
+                     System.out.println(model.getStore().getAlbumInfo(title2));
+                     break;
+                case 4:
+               	 	 System.out.println("Enter artist: ");
+                     String artist2 = scanner.nextLine();
+                     System.out.println(model.getStore().getAlbumInfo(artist2));
+                     break;
+                case 5:
+                     System.out.println("Enter title: ");
+                     String title3 = scanner.nextLine();
+                     System.out.println(model.getSongInfo(title3));
+                     break;
+                case 6: 
+                	 System.out.println("Enter artist: ");
+                     String artist3 = scanner.nextLine();
+                     System.out.println(model.getSongInfo(artist3));
+                     break;
+                case 7:
+                	 System.out.println("Enter title: ");
+                     String title4 = scanner.nextLine();
+                     System.out.println(model.getAlbumInfo(title4));
+                     break;
+                case 8:
+               	 	System.out.println("Enter artist: ");
+                    String artist4 = scanner.nextLine();
+                    System.out.println(model.getAlbumInfo(artist4));
+                    break;
+                default: 
+              	  	System.out.println("Invalid input"); 
+            }
+        }
+    }
+    
+    public void displayWelcomeMessage() {
+    	System.out.println("Welcome to your library.");
+    }
+    
+    
+    public void handleAddMenu() {
+    	while (true) {
+    	displayAddMenu();
+    	System.out.println("Enter your choice: ");
+        int choice = scanner.nextInt();
+        scanner.nextLine(); // consume the leftover newline character
+        
+        switch (choice) {
+        	case 0: 
+        		return;
+        	case 1: 
+        		System.out.println("Enter song name: ");
+                String song = scanner.nextLine();
+                model.addSongToLibrary(song);
+                break;
+        	case 2:
+        		System.out.println("Enter album name: ");
+                String album = scanner.nextLine();
+                model.addAlbumToLibrary(album);
+                break;
+        	default: 
+           	  	System.out.println("Invalid input"); 
+    }
+}
+    }
+    
+   public void handleListMenu() {
+	   
+	   while (true) {
+	   displayListMenu();
+	   System.out.println("Enter your choice: ");
+       int choice = scanner.nextInt();
+       scanner.nextLine(); // consume the leftover newline character
+       
+	   switch (choice) {
+	   case 0:
+		   return;
+	   
+	   case 1: 
+			System.out.println(model.listOfItems("Songs")); break;
+	   case 2:
+			System.out.println(model.listOfItems("Artists")); break;
+	   case 3:
+			System.out.println(model.listOfItems("Albums")); break;
+	   case 4:
+			System.out.println(model.listOfItems("Playlists")); break;
+	   case 5:
+			System.out.println(model.listOfItems("Favorites")); break;
+	   default: 
+     	  System.out.println("Invalid input"); 
+   }
+}
+   }
+   
+   
+   public void handlePlaylistMenu() {
+	   
+	   while (true) {
+	   displayPlaylistMenu();
+	   System.out.println("Enter your choice: ");
+       int choice = scanner.nextInt();
+       scanner.nextLine(); // consume the leftover newline character
+       
+       switch (choice) {
+	  
+	   case 1: 
+		   System.out.println("What would you like to name your playlist?");
+           String name = scanner.nextLine();
+           model.createNewPlaylist(name);
+           break;
+	   case 2:
+		   System.out.println("Name of song you want to add?");
+           String name2 = scanner.nextLine();
+           
+           System.out.println("Name of playlist you want to add to?");
+           String name3 = scanner.nextLine();
+		   model.addSongToPlaylist(name3, name2);
+		   break;
+	   case 3:
+		   System.out.println("Name of song you want to remove?");
+           String name4 = scanner.nextLine();
+           
+           System.out.println("Name of playlist you want to remove it from?");
+           String name5 = scanner.nextLine();
+           model.removeSongFromPlaylist(name5, name4);
+           break;
+	   case 4:
+		   System.out.println("Enter playlist name: ");
+           String playlist = scanner.nextLine();
+       	   System.out.println(model.searchForPlaylist(playlist));
+       	   break;
+	   case 0:
+			return;
+	   default: 
+     	  System.out.println("Invalid input"); 
+       }
+   }
+   	}
+   
+   public void handleSongActionsMenu() {
+	   
+	   while (true) {
+	   displaySongActionsMenu();
+	   System.out.println("Enter your choice: ");
+       int choice = scanner.nextInt();
+       scanner.nextLine(); // consume the leftover newline character
+       
+       switch (choice) {
+           case 0: 
+        	   return;
+           case 1:
+        	   System.out.println("Name of song you want to mark as favorite?");
+               String name = scanner.nextLine();
+        	   model.markSongAsFavorite(name);
+        	   break;
+           case 2: 
+        	   System.out.println("Name of song you want to rate?");
+               String name2 = scanner.nextLine();
+               
+               System.out.println("Rating 1-5 you want to assign it?");
+               int rating = scanner.nextInt();
+               
+               model.rateSong(name2, rating);
+               break;
+          default: 
+        	  System.out.println("Invalid input"); 
+       }
+   }
+   }
+   
 }
