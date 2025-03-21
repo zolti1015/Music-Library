c63d3a33190b245fe557345a50a437ea0322aa86
zolti1015 <zkotha@arizona.edu>
2025-03-21 13:52:06 -0700

    Update Main.java
    altered to no longer create one library model, since we have multiple users. Just simple view object now.

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 import java.io.IOException;
 
+
 public class Main {
+	
   public static void main(String[] args) throws IOException {
-    LibraryModel library = new LibraryModel();
-    View view = new View(library);
+    View view = new View();
     view.run();
   }
 }
